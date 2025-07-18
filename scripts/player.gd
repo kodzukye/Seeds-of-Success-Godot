8921f4256241a5d96a1d310013ce1f3c7f4c4514
kodzukye <lpark9142@gmail.com>
2025-07-18 18:30:04 -0400

    Player movement all axis

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,21 +7,50 @@ const JUMP_VELOCITY = -400.0
 @onready var hair_sprite_animation: AnimatedSprite2D = $HairSpriteAnimation
 @onready var tool_sprite_animation: AnimatedSprite2D = $ToolSpriteAnimation
 
+var direction : Vector2
+
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("move_left", "move_right")
+	# Get horizontal direction
+	var direction1 := Input.get_axis("move_left", "move_right")
 	
-	if direction > 0:
+	# Check movements inputs
+	if Input.is_action_pressed("move_up"):
+		direction = Vector2.UP
+	elif Input.is_action_pressed("move_down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_pressed("move_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("move_right"):
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
+	
+	# Flipping the sprite
+	if direction == Vector2.RIGHT: # Right
 		base_sprite_animation.flip_h = false
 		hair_sprite_animation.flip_h = false
 		tool_sprite_animation.flip_h = false
-	elif direction < 0:
+	elif direction == Vector2.LEFT: # Left
 		base_sprite_animation.flip_h = true
 		hair_sprite_animation.flip_h = true
 		tool_sprite_animation.flip_h = true
-	if direction:
-		velocity.x = direction * SPEED
+		
+	# Play animations
+	if direction == Vector2.ZERO:
+		base_sprite_animation.play("idle_base")
+		hair_sprite_animation.play("idle_hair")
+		tool_sprite_animation.play("idle_tool")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		base_sprite_animation.play("walking_base")
+		hair_sprite_animation.play("walking_hair")
+		tool_sprite_animation.play("walking_tool")
+		
+	# Apply the speed
+	velocity = direction * SPEED
+	"""if direction1:
+		velocity.x = direction1 * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)"""
 
 	move_and_slide()
